59a8b6386b85b7495f043505a558ffe0802d9769
bejaouiamin <127449272+bejaouiamin@users.noreply.github.com>
2025-11-16 22:27:36 +0100

    Add formateur-service configuration and updates

---
+++ src/main/java/com/training/formateur/Response/FormateurResponse.java
@@ -0,0 +1,26 @@
+package com.training.formateur.Response;
+
+import com.training.formateur.entites.ActiviteFormateur;
+import com.training.formateur.entites.DisponibiliteJour;
+import com.training.formateur.entites.StatutFormateur;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+public record FormateurResponse(
+        String id,
+        String nom,
+        String prenom,
+        String email,
+        String telephone,
+        List<String> specialites,
+        List<String> certifications,
+        Integer experienceAnnees,
+        StatutFormateur statut,
+        Map<Integer, DisponibiliteJour> disponibilites,
+        LocalDateTime dateCreation,
+        LocalDateTime dateModification,
+        List<ActiviteFormateur> historique
+) {
+}
---
+++ src/main/java/com/training/formateur/exception/FormateurNotFoundException.java
@@ -0,0 +1,11 @@
+package com.training.formateur.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class FormateurNotFoundException extends RuntimeException {
+    private final String msg;
+}
